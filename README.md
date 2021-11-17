67265ed72a586ba0e0468635532a2b4aafd13bfd
asmsahol <sahol.bin.mijan@gmail.com>
2021-11-18 00:32:05 +0600

    completed site

---
+++ README.md
@@ -0,0 +1,37 @@
+<!-- @format -->
+
+# This is Server Site od Baby Lotion Website
+
+This is Travel Website. The Site Name "Baby Lotion" The Live Site Link is "https://bd-travel-c4f84.web.app/".
+
+## Best Baby Lotion
+
+Best Baby Lotion for All Skin Types As a new mom, it’s natural to only want the best for baby’s skin. Read our quick guide to make sure you pick the best baby lotion for your baby's skin type.
+
+# Baby’s skin is much more
+
+Baby’s skin is much more sensitive and delicate than yours, and seeing your little one suffer through skin irritations can be disheartening. Here at The Bump, we have created a quick guide to the best baby lotions as well as our top picks for different skin ailments. This guide will help you choose which lotion is best for baby’s skin.
+
+## So what is baby lotion for
+
+So what is baby lotion for? Many new moms wonder, When can you put lotion on a baby? Can you put lotion on a newborn? Newborn babies generally do not need to be moisturized by store-bought products—their delicate skin is prone to irritations. Baby skin, however, does require some protection and needs to be moisturized to retain hydration, especially after baths. Dermatologists and pediatricians agree; the best baby lotion depends on your personal preference as well as the type of skin baby has.
+
+## You can choose from creams
+
+You can choose from creams, which do a great job of deep moisturizing without the greasiness of ointments, or you can choose lotions, which absorb very quickly into baby’s skin for overall repair and protection. Be aware of any allergies baby may have to certain ingredients in baby lotions, which can cause rashes and skin irritations when used. Plant-based oils and extracts are common irritants that can inflame baby’s skin. Here are some things to look for on the label when shopping for the best baby lotion for your family:
+
+## Free of Dye and Synthetic Fragrances
+
+Free of Dye and Synthetic Fragrances. Especially for newborns, a natural unscented baby lotion should be used.
+
+## Free of Paraben
+
+Free of Paraben. This is a well-known endocrine disruptor. Free of Phenoxyethanol. This is well known as a suspected carcinogen. Free of preservatives. These are the number-one cause of skin irritations and allergic reactions.
+
+## Plant-based oils
+
+Plant-based oils that are organically extracted.
+
+## Best Baby Lotion
+
+Best Baby Lotion – Overall If baby has normal skin that’s not too sensitive or eczema prone, you can count your blessings and take advantage of some low-cost baby lotions that are still great for his or her skin. You can usually find these at any drugstore or supermarket without having to do prolonged research and trial and error on baby’s delicate skin. Just keep in mind to stay away from baby lotions that have a lot of chemicals that can be toxic to your little one.
--- index.js
+++ index.js
@@ -1,1 +1,185 @@
 /** @format */
+
+const express = require("express");
+const { MongoClient, Collection } = require("mongodb");
+require("dotenv").config();
+const cors = require("cors");
+const ObjectId = require("mongodb").ObjectId;
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middlewere
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.apmnd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("baby_lotion");
+    const productsCollection = database.collection("products");
+    const babyCareCollection = database.collection("baby_care");
+    const usersCollection = database.collection("users");
+    const reviewsCollection = database.collection("reviews");
+    const bookingCollection = database.collection("booking");
+
+    // Get Products API
+    app.get("/products", async (req, res) => {
+      const cursor = productsCollection.find({});
+      const products = await cursor.toArray();
+      res.send(products);
+    });
+    app.get("/baby_care", async (req, res) => {
+      const cursor = babyCareCollection.find({});
+      const babyCare = await cursor.toArray();
+      res.send(babyCare);
+    });
+
+    app.get("/reviews", async (req, res) => {
+      const cursor = reviewsCollection.find({});
+      const reviews = await cursor.toArray();
+      res.send(reviews);
+    });
+
+    app.get("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      let isAdmin = false;
+      if (user?.role === "admin") {
+        isAdmin = true;
+      }
+      res.json({ admin: isAdmin });
+    });
+
+    app.get("/booking", async (req, res) => {
+      const cursor = bookingCollection.find({});
+      const booking = await cursor.toArray();
+      res.send(booking);
+    });
+
+    // Get Single Booking
+    app.get("/booking/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const order = await bookingCollection.findOne(query);
+      res.send(order);
+    });
+
+    // Post to API
+    app.post("/users", async (req, res) => {
+      const result = await usersCollection.insertOne(req.body);
+      console.log(users);
+      res.json(result);
+    });
+
+    app.post("/products", async (req, res) => {
+      const result = await productsCollection.insertOne(req.body);
+      res.send(result);
+    });
+
+    app.post("/reviews", async (req, res) => {
+      const result = await reviewsCollection.insertOne(req.body);
+      res.json(result);
+    });
+
+    app.put("/users", async (req, res) => {
+      const user = req.body;
+      const filter = { email: user.email };
+      const options = { upsert: true };
+      const updateDoc = { $set: user };
+      const result = await usersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.json(result);
+    });
+
+    app.put("/users/admin", async (req, res) => {
+      const user = req.body;
+      const filter = { email: user.email };
+      const updateDoc = { $set: { role: "admin" } };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.json(result);
+      console.log(result);
+    });
+
+    app.post("/booking", async (req, res) => {
+      const result = await bookingCollection.insertOne(req.body);
+      res.send(result);
+    });
+
+    // Update API
+    // // Update Booking Status
+    app.put("/updateStatus/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedStatus = req.body.status;
+      console.log(updatedStatus);
+      const filter = { _id: ObjectId(id) };
+      console.log(updatedStatus);
+      const result = await bookingCollection
+        .updateOne(filter, {
+          $set: { status: updatedStatus },
+        })
+        .then(result => {
+          res.send(result);
+        });
+    });
+
+    // Update Product
+    app.put("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const updateProduct = req.body;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          img: updateProduct.img,
+          name: updateProduct.name,
+          description: updateProduct.description,
+          price: updateProduct.price,
+        },
+      };
+      const result = await productsCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      console.log("Updating", req);
+      res.json(result);
+    });
+
+    // Delete API
+    app.delete("/booking/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await bookingCollection.deleteOne(query);
+      res.json(result);
+    });
+
+    app.delete("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productsCollection.deleteOne(query);
+      res.json(result);
+      console.log(result);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("The server in running.");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
